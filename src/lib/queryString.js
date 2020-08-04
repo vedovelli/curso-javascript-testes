6c65dd6efa65c1d849c3ee1f265e113692e930c1
Vedovelli <vedovelli@hey.com>
2020-08-04 07:41:44 +0200

    Mais refactoring

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,9 +1,9 @@
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please check yout params');
+  }
+  return `${key}=${value}`;
+};
+
 module.exports.queryString = obj =>
-  Object.entries(obj)
-    .map(([key, value]) => {
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        throw new Error('Please check yout params');
-      }
-      return `${key}=${value}`;
-    })
-    .join('&');
+  Object.entries(obj).map(keyValueToString).join('&');
