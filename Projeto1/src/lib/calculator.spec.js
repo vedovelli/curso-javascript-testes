eb0cc19a3612517a07901ab8b2d655404f145465
Vedovelli <vedovelli@hey.com>
2020-08-12 07:30:50 +0200

    Criação pasta para todos projetos

---
+++ Projeto1/src/lib/calculator.spec.js
@@ -0,0 +1,27 @@
+import { sum } from './calculator';
+
+it('should sum 2 and 2 and the result must be 4', () => {
+  expect(sum(2, 2)).toBe(4);
+});
+
+it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
+  expect(sum('2', '2')).toBe(4);
+});
+
+it('should throw an error if what is provided to the method cannot be summed', () => {
+  expect(() => {
+    sum('', 2);
+  }).toThrowError();
+
+  expect(() => {
+    sum([2, 2]);
+  }).toThrowError();
+
+  expect(() => {
+    sum({});
+  }).toThrowError();
+
+  expect(() => {
+    sum();
+  }).toThrowError();
+});
